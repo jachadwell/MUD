817ee95af0690a92aeb7e075e52435896e803452
Jared Chadwell <jared.alan.chadwell@gmail.com>
2020-08-31 11:52:55 -0400

    Added websocket
    Added npm package socket.io to server and socket.io-client to client

--- Client/mud/src/App.js
+++ Client/mud/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import socketIOClient from "socket.io-client";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+var socket;
+
+export default class FrontPage extends React.Component{
+  
+  constructor(props){
+    super();
+    this.state = {
+      endpoint: "localhost:5000"
+    }
+    socket = socketIOClient(this.state.endpoint);
+  }
+
+  componentDidMount(){
+    //this.setState();
+    console.log("mounted")
+    socket.emit("connection");
+  }
+
+  render(){
+    console.log("render");
+    return(
+      <div>Test</div>
+    )
+  }
 }
-
-export default App;
--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Server running...');
 })
 
-app.listen(port, console.log("Server listening on port: " + port));
+
+io.on('connection', (socket) => {
+    console.log('USER CONNECTED:');
+    console.log(socket.id);
+})
+
+server.listen(port, console.log("Server listening on port: " + port));
